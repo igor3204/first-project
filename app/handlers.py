cd521e4abafe9c4354271ac68720dc5be90b5c50
matvey.p <matvey_potapushin364@proton.me>
2024-12-12 23:50:27 +0300

    new keyboard inline,  dick sizer handler

--- app/handlers.py
+++ app/handlers.py
@@ -1,8 +1,8 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
-
-from app.keybord import main
+from aiogram.types import Message, CallbackQuery
+import random
+from app.keybord import get_sizer_digs_keyboard, main
 
 router = Router()
 
@@ -19,4 +19,16 @@ async def get_help(message: Message):
 
 @router.message(F.text == "хуй")
 async def poslan(message: Message):
-    await message.answer("нет")
+    kb = get_sizer_digs_keyboard()
+    await message.answer("нет", reply_markup=kb)
+
+
+@router.message(F.text == "Отсосать")
+async def suck_dick_minet(message: Message):
+    await message.answer("глотай, друже, потужной минет!")
+
+
+@router.callback_query(F.data == "my_dick_size")
+async def get_size_my_dick(callback_data: CallbackQuery):
+    dick_size = random.randint(1, 100)
+    await callback_data.message.answer(f"Твой размер члена: {dick_size} см")
--- app/keybord.py
+++ app/keybord.py
@@ -1,4 +1,4 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 main = ReplyKeyboardMarkup(
     keyboard=[
@@ -9,3 +9,10 @@ main = ReplyKeyboardMarkup(
     resize_keyboard=True,
     input_field_placeholder="Выберите пункт",
 )
+
+def get_sizer_digs_keyboard() -> InlineKeyboardMarkup:
+    main_inline_kb = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Получи размер своего члена", callback_data="my_dick_size")],
+    ])
+    
+    return main_inline_kb
