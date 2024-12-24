68cfa38d0d323d1466387c3b7ed03dea2e2c105e
jeyms <terremokvasi@gmail.com>
2024-12-24 18:21:09 +0300

    gfg

--- app/handlers.py
+++ app/handlers.py
@@ -1,7 +1,7 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery
-import random
+from random import randint
 from app.keybord import get_sizer_digs_keyboard, main
 
 router = Router()
@@ -9,28 +9,4 @@ router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.reply("s", reply_markup=main)
-
-
-@router.message(Command("help"))
-async def get_help(message: Message):
-    await message.answer("gg")
-
-
-@router.message(F.text == "хуй")
-async def poslan(message: Message):
-    kb = get_sizer_digs_keyboard()
-    await message.answer("нет", reply_markup=kb)
-
-
-@router.message(F.text == "Отсосать")
-async def suck_dick_minet(message: Message):
-    await message.answer("глотай, друже, потужной минет!")
-
-
-@router.callback_query(F.data == "my_dick_size")
-async def get_size_my_dick(callback_data: CallbackQuery):
-    dick_size = random.randint(5, 14)
-    await callback_data.message.answer(
-        f"Твой размер члена: {dick_size} см, не расстраивайся, размер не главное!"
-    )
+    pass
--- app/keybord.py
+++ app/keybord.py
@@ -1,18 +1,6 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-
-main = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="Меню")],
-        [KeyboardButton(text="Отсосать")],
-        [KeyboardButton(text="Экстренная помощь")],
-    ],
-    resize_keyboard=True,
-    input_field_placeholder="Выберите пункт",
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
 )
-
-def get_sizer_digs_keyboard() -> InlineKeyboardMarkup:
-    main_inline_kb = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Получи размер своего члена", callback_data="my_dick_size")],
-    ])
-    
-    return main_inline_kb
